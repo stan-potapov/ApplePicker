3b86a6cdb4e1d0e7c2401eb7f2450a509892064b
Stan Potapov <stan.potapov98@gmail.com>
2020-02-27 20:48:01 -0500

    finished apple picker

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Apple : MonoBehaviour {
+	[Header("Set in Inspector")]
 
     public static float bottomY = -20f;                                 
 
     void Update () {
-        if ( transform.position.y < bottomY ) {
-            Destroy( this.gameObject );                                      
+        if (transform.position.y < bottomY) {
+            Destroy(this.gameObject);
+             // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();
         }
     }
 }
